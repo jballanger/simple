3ec2860e07ba15c694f9759230df7b224b2c7f9a
Julien <ballanger.julien@gmail.com>
2016-07-20 13:52:27 +0200

    Adding catch for database connect

--- lib/class.DBFactory.php
+++ lib/class.DBFactory.php
@@ -1,12 +1,19 @@
 <?php
-  class DBFactory
-  {
-    public static function getPDO()
-    {
-      $db = new PDO('mysql:host=localhost; dbname=simple', 'root', '');
-      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-      return $db;
-    }
-  }
+	class DBFactory
+	{
+    	public static function getPDO()
+    	{
+    		try
+    		{
+				$db = new PDO('mysql:host=localhost; dbname=simple', 'root', '');
+	      		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	      		return $db;
+	      	}
+	      	catch(PDOException $e)
+	      	{
+	      		echo "/!\\ Unable to connect to the database, please contact the Administrator /!\\";
+	      		exit();
+	      	}
+    	}
+  	}
 ?>
